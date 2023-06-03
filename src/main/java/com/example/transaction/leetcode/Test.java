263ed1f52075d47a653d9ea96476dfda5fa90ceb
777 <goszakupki.aktiv@gmail.com>
2023-06-03 19:02:40 +0300

    6661

--- src/main/java/com/example/transaction/leetcode/Test.java
+++ src/main/java/com/example/transaction/leetcode/Test.java
@@ -2,18 +2,28 @@ package com.example.transaction.leetcode;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class Test {
 
     public static void main(String[] args) {
 
-        List<Object> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 99, -999, -777));
+        Test test = new Test();
+        List<Object> objectsList = new ArrayList<>(Arrays.asList("StringMem", 666, "Lol"));
+        test.mem(objectsList);
 
-        list.add("lol");
-        list.add(55);
-        list.add(new BigInteger("777777777777777777777777"));
-        System.out.println(list);
 
     }
+
+    public void lol(List<? extends Number> list){
+        System.out.println(list);
+    }
+
+    public void mem(List<? super Integer> list){
+        System.out.println(list);
+    }
+
 }

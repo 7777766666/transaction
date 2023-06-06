cbc2ea36b929a3da4a1404adbdd4449637428250
777 <goszakupki.aktiv@gmail.com>
2023-06-06 23:52:24 +0300

    ннн

--- src/main/java/com/example/transaction/leetcode/Ppp.java
+++ src/main/java/com/example/transaction/leetcode/Ppp.java
@@ -11,6 +11,9 @@ import lombok.Setter;
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Getter
 @Setter
@@ -34,8 +37,23 @@ public class Ppp {
     public static void main(String[] args) {
         Ppp black = new Ppp(44, "Black", 1000);
         Ppp white = new Ppp(22, "White", 5000);
+        Ppp red = new Ppp(11, "Red", 2500);
+        Ppp yellow = new Ppp(11, "Yellow", 1500);
 
-        ArrayList<Ppp> list = new ArrayList<>(Arrays.asList(black, white));
+
+
+
+        ArrayList<Ppp> list = new ArrayList<>(Arrays.asList(black, white, red, yellow));
+
+        int result = list.stream()
+                .filter(x -> x.num > 100)
+                .filter(i -> i.id > 1)
+                .reduce(999, (Integer, Ppp) -> Integer + Ppp.num, Integer::sum);
+
+//        collect.forEach(System.err::println);
+        System.out.println(result +" result");
+
+
 
         list.forEach((lol) -> lol.setNum(lol.getNum()* 7)) ;
 
@@ -57,7 +75,18 @@ int y = 88;
         System.out.println(sss +" sss");
 
         Gop <String, String, Integer> gopStop = (a765, b936) -> Integer.parseInt(a765) * Integer.parseInt(b936);
-        System.err.println(gopStop.ddd("12", "8") + " ddd");
+        System.out.println(gopStop.ddd("12", "8") + " ddd");
+
+        Stop <String , String, Integer> xaxa = (ccc, cccc) -> Integer.parseInt(ccc) - Integer.parseInt(cccc);
+        Integer st = xaxa.st("29", "-100");
+        System.out.println(st + "  I thint plus 129");
+
+
+    }
+
+    interface Stop<String, String1, Integer>{
+        java.lang.Integer st(java.lang.String w, java.lang.String t);
+
     }
     @FunctionalInterface
     interface Gop<T, T1, R>{
